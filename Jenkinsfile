c02dd3a8c093b6ddc2f52c28f7e67fad9fc15510
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-12-10 01:10:17 +0900

    Dockerfile追加。「デプロイ先のサーバ」のイメージ、スクリプト群を作成。

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,7 +5,11 @@ node {
   }
 
   stage('Build docker image "Java9 compile env"') {
-    sh "docker build -t oracle-java/java9-plus - < ./src/docker/java9-plus/Dockerfile"
+    sh "docker build -t oracle-java9-plus - < ./src/docker/java9-plus/Dockerfile"
+  }
+
+  stage('Build docker image "Deploy target"') {
+    sh "docker build -t deploy-target - < ./src/docker/deploy-target/Dockerfile"
   }
 
 }
---
+++ src/docker/deploy-target/scripts/startup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# デタッチ時に走ることを期待したスクリプト。
+# 「最初に一回」の処理に対しては冪等性を考慮し、記述すること。
+# author : kazuhito_m
+
+# サービス開始(二回目以降はリスタート)
+service ssh restart
+
+# MySql再起動
+
+# DBが行方不明だっつーエラーを吐きやがるので、事前に教ええる
+mysql_install_db --datadir=/var/lib/mysql --user=mysql
+service mysql restart
+
+# Dockerfileサイドで「root:rootをパスワードに設定」ってやってるはずなのに…rootをnonpassで通しやがる。TODOや
+
+# mysqlに外から入れるよう設定(二回目以降も権限系だけはふり直す)
+mysql --user=root -e 'CREATE DATABASE IF NOT EXISTS odf_edit_sample;'
+mysql --user=root -e 'GRANT ALL PRIVILEGES ON *.* TO databaseuser@"%.%.%.%" IDENTIFIED BY "databasepassword";'
+mysql --user=root -e 'GRANT ALL PRIVILEGES ON *.* TO databaseuser@"localhost" IDENTIFIED BY "databasepassword";'
+mysql --user=root -e 'GRANT ALL PRIVILEGES ON *.* TO root@"%.%.%.%" IDENTIFIED BY "root";'
+mysql --user=root -e 'GRANT ALL PRIVILEGES ON *.* TO root@"localhost" IDENTIFIED BY "root";'
+
+# Dockerの外をマウントしているのを期待しているディレクトリ `/tmp/app_import` から、SpringBootのJarを取得する。
+mkdir -p /tmp/deploy
+cp /tmp/app_import/*.jar /tmp/deploy/app.jar
+
+# SpringBootアプリ起動(エンドレス起動兼ねる)
+java -jar /tmp/deploy/app.jar
